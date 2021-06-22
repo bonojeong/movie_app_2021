e64dbb965eba82d23e63ac2d60190f9bc5ce7bee
bonojeong <by1619@hanmail.net>
2021-06-22 21:02:50 +0900

    react 수업

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import PropTypes from "prop-types";
+
+const foodILike = [
+  {
+    id:1,
+    name:"kimchi",
+    rating: 3,
+    price: 3000
+  },
+  {
+    id:2,
+    name:"ice",
+    rating: 4,
+    price: 3000
+  },
+  {
+    id:3,
+    name:"jeon",
+    rating: 5,
+    price: 3000
+  },
+  {
+    id:4,
+    name:"bap",
+    rating: 2,
+    price: 3000
+  },
+  {
+    id:5,
+    name:"kim",
+    rating: 5,
+    price: 3000
+  }
+]
+
+function Food({name, OfPrice, rating }) {
+  return (
+    <div>
+      <h2>I like {name}</h2>
+      <h4>{rating}/5</h4>
+      <h4>Price of the dish is {OfPrice}</h4>
+    </div>
+  )
+}
+
+//isRequired는 무조건 있어야한다!! 만약에 없으면 에러뜸 ! 
+//isRequired를 안적으면 object안에 없어도됨
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  OfPrice: PropTypes.number.isRequired,
+  rating: PropTypes.number.isRequired
+}
+
+function renderFood(dish){
+  console.log(dish)
+  return <Food key={dish.id} name={dish.name} OfPrice={dish.price} rating = {dish.rating} />
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {foodILike.map( renderFood )}
     </div>
   );
 }
